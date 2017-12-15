24b85bc8ef2377b0725d05b0ecbbb98fb4946096
Andrew Piper <piper.andrew@spcollege.edu>
2017-12-15 10:49:34 -0500

    Added solution, license, gitignore and build script.

---
+++ BuildNugetPackages.bat
@@ -0,0 +1,11 @@
+set BASE=%~dp0
+set SEARCH=%BASE%
+set OUT=%BASE%nuget
+rmdir %OUT%
+mkdir %OUT%
+pushd %BASE%
+dotnet build -c Release
+pushd %SEARCH%
+FOR /R %SEARCH% %%I in (Release\*.nupkg) DO copy %%I %OUT%
+popd
+popd
